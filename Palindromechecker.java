653c821fd79f71647018fddb7b9d4e5ecf1e3ab7
Herok4Build <thomash.johnson261@gmail.com>
2018-08-21 21:42:33 -0400

    Better looking palindrome code.

--- Palindromechecker.java
+++ Palindromechecker.java
@@ -4,36 +4,40 @@ import java.util.*;
 import java.lang.*;
 public class Palindromechecker
 {
-public static void main(String args[])
-{
+    public static void main(String args[])
+    {
 
-System.out.printf("Please enter a word to be checked.\n");
+        System.out.printf("Please enter a word to be checked.\n");
 
-Scanner p = new Scanner(System.in);
+        Scanner p = new Scanner(System.in);
 
-String palindrome = p.nextLine();
+        String palindrome = p.nextLine();
 
-System.out.printf("%s is the word to be checked.\n", palindrome);
+        System.out.printf("%s is the word to be checked.\n", palindrome);
 
-char[] palindrometest = palindrome.toCharArray();
+        char[] palindrometest = palindrome.toCharArray();
 
-int point_1= 0;
-int point_2= palindrometest.length-1;
-boolean checkfalse= true;
+        int point_1= 0;
+        
+        int point_2= palindrometest.length-1;
+        
+        boolean checkfalse= true;
 
-for(point_1 = 0; point_1<point_2; ++point_1, --point_2)
-{
-if (palindrometest[point_1] != palindrometest[point_2])
-{
-    System.out.printf("%s is not a palindrome", palindrome);
-    checkfalse = false;
-    break;
-}
-checkfalse= true;
-}
-if (checkfalse==true)
-{
-System.out.printf("%s is a palindrome.\n", palindrome);
-}
-}
+        for(point_1 = 0; point_1<point_2; ++point_1, --point_2)
+        {
+            if (palindrometest[point_1] != palindrometest[point_2])
+            {
+                System.out.printf("%s is not a palindrome. \n", palindrome);
+                
+                checkfalse = false;
+                
+                break;
+            }
+            checkfalse= true;
+        }
+        if (checkfalse==true)
+            {
+                System.out.printf("%s is a palindrome.\n", palindrome);
+            }
+    }
 }
